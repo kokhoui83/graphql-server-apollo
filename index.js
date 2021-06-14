735b93d52c2ed167ed33da306c45f0ad5676376f
brian lim <kokhoui.lim@duerrdental.com>
2021-06-15 07:08:05 +0800

    remove ;

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { ApolloServer, gql, PubSub } = require('apollo-server');
+const { ApolloServer, gql, PubSub } = require('apollo-server')
 
 // A schema is a collection of type definitions (hence "typeDefs")
 const typeDefs = gql`
